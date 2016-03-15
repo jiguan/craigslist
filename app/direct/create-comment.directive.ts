fe7b7210268fc044c33cb18a75d6e086b24964c1
Ricky Guan <ricky.guan@cookmedical.com>
2016-03-15 14:34:13 -0400

    Now I can add posts, comments dynamically. But, don't know how to interact with database. Do I need to reload the posts by calling postService again?

--- app/direct/create-comment.directive.ts
+++ app/direct/create-comment.directive.ts
@@ -1,26 +1,34 @@
 import { Component, Directive } from 'angular2/core';
 import { Collapse } from './collapse';
+import { Comment } from '../model/comment';
+
 @Component({
     selector: 'create-comment',
   template: `
   <button class="btn btn-default" (click)="isCollapsed = !isCollapsed">Add a comment</button>
-  <div [collapse]="isCollapsed" class="form-control collapse my-form">
-      <form>
-          <div class="form-group">
-            <label for="alterEgo">Comment: </label>
-            <input type="text" class="form-control" ><!--[(ngModel)]="post.title" -->
-          </div>
-         <button type="submit" class="btn btn-default" (click)="submit(comment)">
-             <span class="glyphicon glyphicon-ok"></span> Submit
-         </button>
-         <button class="btn btn-default" (click)="isCollapsed = !isCollapsed">
-              <span class="glyphicon glyphicon-arrow-left"></span> Cancel
-         </button>
-      </form>
-  </div>
+  <form (ngSubmit)="onSubmit()" [collapse]="isCollapsed" class="form-control collapse my-form">
+      <div class="form-group">
+        <label for="alterEgo">Comment: </label>
+        <input type="text" class="form-control" [(ngModel)]="model.comment">
+      </div>
+     <button type="submit" class="btn btn-default">
+         <span class="glyphicon glyphicon-ok"></span> Submit
+     </button>
+     <button class="btn btn-default" (click)="isCollapsed = !isCollapsed">
+          <span class="glyphicon glyphicon-arrow-left"></span> Cancel
+     </button>
+  </form>
   `,
- directives: [Collapse]
+ directives: [Collapse],
+  inputs: ['comments']
 })
 export class CreateCommentDirective {
     public isCollapsed:boolean = true;
+    submitted = false;
+    model = new Comment();
+    comments: Comment[];
+    onSubmit() {
+        this.isCollapsed = true;
+        this.comments.push(this.model);
+    }
 }
--- app/direct/create-post.directive.ts
+++ app/direct/create-post.directive.ts
@@ -1,34 +1,43 @@
 import { Component, Directive } from 'angular2/core';
 import { Collapse } from './collapse';
+import { Post } from '../model/post';
+//http://valor-software.com/ng2-bootstrap/
 @Component({
     selector: 'create-post',
 // })
 // //https://angular.io/docs/ts/latest/guide/attribute-directives.html
 // @View({
+//https://angular.io/docs/ts/latest/guide/forms.html
   template: `
   <button id="new-post-btn" class="btn btn-default" (click)="isCollapsed = !isCollapsed">Create a new post</button>
-  <div [collapse]="isCollapsed" class="form-control collapse my-form">
       {{diagnostic}}
-      <form>
-          <div class="form-group">
-            <label for="alterEgo">Title: </label>
-            <input type="text" class="form-control" ><!--[(ngModel)]="post.title" -->
-          </div>
-          <div class="form-group">
-            <label for="alterEgo">Detail: </label>
-            <input type="text" class="form-control">
-          </div>
-         <button type="submit" class="btn btn-default" (click)="submit(post)">
-             <span class="glyphicon glyphicon-ok"></span> Submit
-         </button>
-         <button class="btn btn-default" (click)="isCollapsed = !isCollapsed">
-              <span class="glyphicon glyphicon-arrow-left"></span> Cancel
-         </button>
-      </form>
-  </div>
+  <form (ngSubmit)="onSubmit()" [collapse]="isCollapsed" class="form-control collapse my-form">
+      <div class="form-group">
+        <label for="alterEgo">Title: </label>
+        <input type="text" class="form-control" [(ngModel)]="model.title">
+      </div>
+      <div class="form-group">
+        <label for="alterEgo">Detail: </label>
+        <input type="text" class="form-control" [(ngModel)]="model.detail">
+      </div>
+     <button type="submit" class="btn btn-default">
+         <span class="glyphicon glyphicon-ok"></span> Submit
+     </button>
+     <button class="btn btn-default" (click)="isCollapsed = !isCollapsed">
+          <span class="glyphicon glyphicon-arrow-left"></span> Cancel
+     </button>
+  </form>
   `,
- directives: [Collapse]
+ directives: [Collapse],
+ inputs: ['posts']
 })
 export class CreatePostDirective {
     public isCollapsed:boolean = true;
+    submitted = false;
+    model = new Post();
+    posts: Post[];
+    onSubmit() {
+        this.isCollapsed = true;
+        this.posts.push(this.model);
+    }
 }
