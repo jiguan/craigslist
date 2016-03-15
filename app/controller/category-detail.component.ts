4bf2df74a14085c881f2acfada13c03d6289ace2
Ricky Guan <ricky.guan@cookmedical.com>
2016-03-15 11:39:31 -0400

    Start to move create post and create comments into directives

--- app/controller/category-detail.component.ts
+++ app/controller/category-detail.component.ts
@@ -4,17 +4,21 @@ import { PostService } from '../service/post.service';
 import { RouteParams, Router } from 'angular2/router';
 import { Category } from '../model/category';
 import { Post } from '../model/post';
+import { Collapse } from '../direct/collapse';
+import { CreatePostDirective } from '../direct/create-post.directive';
 
 
 @Component({
 	selector: 'category-detail',
 	templateUrl: 'app/view/category-detail.component.html',
 	// styleUrls: ['app/view/category-detail.component.css']
+	 directives: [Collapse]
 })
 
 export class CategoryDetailComponent implements OnInit {
 	category: Category;
 	posts: Post[];
+	public isCollapsed:boolean = false;
 	constructor(private _router: Router, private _categoryService: CategoryService, private _postService: PostService, private _routeParams: RouteParams) {}
 	ngOnInit() {
 		let id = +this._routeParams.get('id');
